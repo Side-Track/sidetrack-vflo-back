c7046e184c062dde5ffdd360c1b81c827c6a86e4
lunaJYJ <devmonarch0115@gmail.com>
2022-02-10 15:22:01 +0900

    etc: email_verification repository add

---
+++ src/auth/email_verification.repository.ts
@@ -0,0 +1,58 @@
+import { InternalServerErrorException, Logger, NotFoundException } from "@nestjs/common";
+import { ResponseDto } from "src/dto/response.dto";
+import { EntityRepository, Repository } from "typeorm";
+import { EmailVerification } from "./entities/email_verification.entity";
+
+@EntityRepository(EmailVerification)
+export class EmailVerificationRepository extends Repository<EmailVerification> {
+  
+  // verification code generate & create db row
+  async createVerificationCode(email: string): Promise<string> {
+
+    // 코드 생성
+    let code = '';
+
+    // 코드 최소 길이는 6자
+    while(code.length < 6) {
+      code = Math.random().toString(36).slice(2);
+    }
+
+    // 유효 기간 설정 후 DB 에 insert
+    let expiredDate = new Date();
+    expiredDate.setMinutes(expiredDate.getMinutes() + 10);
+    let emailVerificationTuple = this.create({email: email, verification_code: code, expired_date: expiredDate});
+
+    try {
+      await this.save(emailVerificationTuple);
+      return code;
+    } catch(err) {
+      Logger.warn(err);
+      throw new InternalServerErrorException('Internal Server Error is occured. Plz contact administartor.');
+    }
+  }
+
+  // 이메일 인증하기
+  async verifyEmail(email:string, code:string): Promise<boolean> {
+
+    // 이메일과 생성된 코드로 검색
+    const query = this.createQueryBuilder('email_verification');
+    query.where('expired_date >= :currentDate', {currentDate : new Date()})
+    .andWhere('email = :email',{email: email})
+    .andWhere('verification_code = :code', {code: code});
+
+    // 검색 결과
+    let verifyObject = await query.getOne();
+
+    // 검색된 결과가 있으면(발행했던 코드라면)
+    if(verifyObject != undefined) {
+
+      // verified_date update
+      verifyObject.verified_date = new Date();
+      await this.save(verifyObject);
+      return true;
+    } else {
+      // 검색 결과 없으면
+      return false;
+    }
+  }
+}

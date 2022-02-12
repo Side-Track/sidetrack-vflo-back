83d1fd4463f9128e3d17d8de83827f90065d547b
lunaJYJ <devmonarch0115@gmail.com>
2022-02-13 00:38:22 +0900

    init: profile entity 생성

---
+++ src/profile/entities/profile.entity.ts
@@ -0,0 +1,21 @@
+import { User } from 'src/auth/entities/user.entity';
+import { BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['nickname'])
+export class Profile extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	idx: number;
+
+	@OneToOne(() => User)
+	@JoinColumn()
+	user: User;
+
+	@Column()
+	nickname: string;
+
+	@Column()
+	bio: string;
+
+	// 장르 추가
+}

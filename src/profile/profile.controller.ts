6a3acfd2aa0b7b767adff278fa5acca8d160e89e
JangLuna <75708535+JangLuna@users.noreply.github.com>
2022-02-12 21:02:52 +0900

    init: create Profile module (#27)
    * init: create Profile module
    
    * etc: prettier 적용

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ProfileService } from './profile.service';
+
+@Controller('profile')
+@UseGuards(AuthGuard())
+export class ProfileController {
+	constructor(private profileService: ProfileService) {}
+}

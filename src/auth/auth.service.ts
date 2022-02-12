a6d789c1c9b10fae5d503ab18dbacf0f868bfdf8
lunaJYJ <devmonarch0115@gmail.com>
2022-02-12 23:06:25 +0900

    fix: 회원가입 구조 리팩토링

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ResponseDto } from 'src/dto/response.dto';
 import { UserCredentialDto } from './dto/user-credential.dto';
@@ -12,59 +12,53 @@ import { EmailVerificationDto } from './dto/email-verification.dto';
 
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { ResponseCode } from 'src/response.code.enum';
+import { EmailVerification } from './entities/email_verification.entity';
 
 @Injectable()
 export class AuthService {
-  
 	constructor(
 		@InjectRepository(UserRepository)
 		private userRepository: UserRepository,
+
+		@InjectRepository(EmailVerificationRepository)
 		private emailVerficiationRepository: EmailVerificationRepository,
 		private readonly mailerService: MailerService,
 		private jwtService: JwtService,
 	) {}
 
-	// 회원 가입
-	async signUp(userCredentialDto: UserCredentialDto): Promise<ResponseDto> {
-		await this.userRepository.createUser(userCredentialDto);
+	// 중복 이메일 검사
+	async checkDuplicateEmail(email: string): Promise<ResponseDto> {
+		const user = await this.userRepository.findOne({ email });
 
-		return new ResponseDto(constant.HttpStatus.OK, undefined, undefined);
-	}
-
-	// 로그인
-	async signIn(userCredentialDto: UserCredentialDto): Promise<ResponseDto> {
-		// DTO 로 부터 데이터 받음
-		const { email, password } = userCredentialDto;
-
-		// 받은 데이터 기준으로 db 검색
-		const user = await this.userRepository.findOne({ email: email });
-
-		// 요청으로부터 온 비밀번호와 암호화 된 비밀번호 검사
-		const passwordCompareResult = await bcrypt.compare(password, user.password);
-		if (user != undefined && passwordCompareResult) {
-			// 유저 토큰 생성
-			const { idx, email, email_verified } = user;
-			const payload = {
-				idx: idx,
-				email: email,
-				email_verified: email_verified,
-			};
-			const accessToken = await this.jwtService.sign(payload);
-
-			return new ResponseDto(constant.HttpStatus.OK, 'Sign-in success!', { accessToken });
-		} else {
-			throw new UnauthorizedException('Sign-in Failed');
+		if (user) {
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.SUCCESS, false, 'Sign up available', {
+				available: true,
+			});
 		}
+
+		return new ResponseDto(constant.HttpStatus.OK, ResponseCode.ALREADY_REGISTERED_USER, true, 'Duplicate User exist', {
+			available: false,
+		});
 	}
 
 	// 인증 메일 발송
 	async sendVerificationMail(email: string): Promise<ResponseDto> {
+		// 이미 인증받은 유저인지 검사
 		const user = await this.userRepository.findOne({ email: email });
 
-		// 이미 인증된 유저라면 패스
-		if (user.email_verified) {
-			return new ResponseDto(constant.HttpStatus.OK, 'Already verified account.', { verified: user.email_verified });
+		// 이미 인증된 유저라면 response error return
+		if (user && user.email_verified) {
+			return new ResponseDto(
+				constant.HttpStatus.OK,
+				ResponseCode.ALREADY_VERIFIED_ACCOUNT,
+				true,
+				'Already verified account.',
+				{ verified: user.email_verified },
+			);
 		}
+
+		// 인증 받아야 한다면 : 가입은 되었으나 인증은 안된경우와 처음 인증하고 가입절차 밟는 경우
 
 		// 코드를 생성함
 		const verificationCode = await this.emailVerficiationRepository.createVerificationCode(email);
@@ -86,43 +80,97 @@ export class AuthService {
 
 		// 메일 발송 완료 되었다면
 		if (sendedMailResponse.search('OK') && sendedMailReceiver === email) {
-			return new ResponseDto(constant.HttpStatus.OK, 'Email-verification code is generated', undefined);
+			return new ResponseDto(
+				constant.HttpStatus.OK,
+				ResponseCode.SUCCESS,
+				false,
+				'Email-verification code is generated',
+			);
 		}
 	}
 
 	// 이메일 인증
 	async verifyEmail(emailVerificationDto: EmailVerificationDto): Promise<ResponseDto> {
 		// 이메일 인증하기
-		const verified = await this.emailVerficiationRepository.verifyEmail(emailVerificationDto);
-		const { email } = emailVerificationDto;
+		let verifyObject: EmailVerification = await this.emailVerficiationRepository.findAvailableEmailVerification(
+			emailVerificationDto,
+		);
 
-		// 인증 성공
-		if (verified) {
-			// 유저 검색하여 email_verified 를 true로
-			const user = await this.userRepository.findOne({ email: email });
-			if (user != undefined) {
-				user.email_verified = true;
-				await this.userRepository.save(user);
+		// 검색된 결과가 있으면(발행했던 코드라면)
+		if (verifyObject != undefined) {
+			// verified_date update
+			verifyObject.verified_date = new Date();
+			await this.emailVerficiationRepository.save(verifyObject);
 
-				return new ResponseDto(constant.HttpStatus.OK, 'Email is verfied', { verified: verified });
-			}
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.SUCCESS, false, 'email is verified!');
 		} else {
-			// 만약 인증 실패라면
-			return new ResponseDto(constant.HttpStatus.OK, "Can't find any matchs with email, code pair", {
-				verified: verified,
-			});
+			// 검색 결과 없으면
+			return new ResponseDto(
+				constant.HttpStatus.OK,
+				ResponseCode.DATA_NOT_FOUND,
+				false,
+				"Can't find any matchs with email, code data",
+			);
 		}
 	}
 
-	// 중복 이메일 검사
-	async checkDuplicateEmail(email: string): Promise<ResponseDto> {
-		const user = await this.userRepository.count({ email });
+	// 회원 가입
+	async signUp(userCredentialDto: UserCredentialDto): Promise<ResponseDto> {
+		const { email } = userCredentialDto;
 
-		if (user == 0) {
-			return new ResponseDto(constant.HttpStatus.OK, 'Sign up available', { available: true });
+		// 인증받은 이메일인지 판단
+		const verifiedEmail: EmailVerification =
+			await this.emailVerficiationRepository.findVerifiedEmailVerificationByEmail(email);
+
+		// 인증받은 이메일이 아니라면(인증받은 이메일-코드 페어가 없으면)
+		if (!verifiedEmail) {
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.NOT_VERIFIED_EMAIL, true, 'Not Verified Email.');
 		}
 
-		return new ResponseDto(constant.HttpStatus.OK, 'Duplicate User exist', { available: false });
+		// 유저 만들기
+		const user: User = await this.userRepository.createUser(userCredentialDto);
+
+		// 유저 만들기가 모종의 이유로 실패 시
+		if (!user) {
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.ETC, true, 'unspecific error occured.');
+		}
+
+		// 성공 시
+		return new ResponseDto(constant.HttpStatus.OK, ResponseCode.SUCCESS, false, 'Sign-up success!');
+	}
+
+	// 로그인
+	async signIn(userCredentialDto: UserCredentialDto): Promise<ResponseDto> {
+		// DTO 로 부터 데이터 받음
+		const { email, password } = userCredentialDto;
+
+		// 받은 데이터 기준으로 db 검색
+		const user = await this.userRepository.findOne({ email: email });
+
+		// 요청으로부터 온 비밀번호와 암호화 된 비밀번호 검사
+		const passwordCompareResult = await bcrypt.compare(password, user.password);
+
+		// 존재하지 않는 유저일경우
+		if (!user) {
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.WRONG_EMAIL_OR_PASSWORD, true, 'Sign-in Failed');
+		}
+
+		if (passwordCompareResult) {
+			// 유저 토큰 생성
+			const { idx, email, email_verified } = user;
+			const payload = {
+				idx: idx,
+				email: email,
+				email_verified: email_verified,
+			};
+			const accessToken = await this.jwtService.sign(payload);
+
+			// 정상처리. 토큰과 함께 반환
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.SUCCESS, false, 'Sign-in success!', { accessToken });
+		} else {
+			// 비밀번호가 다를경우
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.WRONG_EMAIL_OR_PASSWORD, true, 'Sign-in Failed');
+		}
 	}
 
 	// 비밀번호 리셋
@@ -131,10 +179,17 @@ export class AuthService {
 
 		// 해당 이메일 계정이 없으면
 		if (user == undefined) {
-			return new ResponseDto(constant.HttpStatus.DATA_NOT_FOUND, "Can't find any account with email", {});
+			return new ResponseDto(
+				constant.HttpStatus.OK,
+				ResponseCode.NOT_REGISTERED_USER,
+				true,
+				"Can't find any account with email",
+			);
 		}
 
-		// 계정이 있다면
+		// 계정이 있다면 ============
+
+		// 임시비밀번호 생성
 		const tempPassword = await this.userRepository.createTemporaryPassword(user);
 
 		// 이메일 내용 작성
@@ -154,17 +209,9 @@ export class AuthService {
 
 		// 메일 발송 완료 되었다면
 		if (sendedMailResponse.search('OK') && sendedMailReceiver === email) {
-			const response = new ResponseDto(constant.HttpStatus.OK, 'Temporary Password is sented.', undefined);
-			response.setCode('SUCCESS');
-			response.setError(false);
-			return response;
+			return new ResponseDto(constant.HttpStatus.OK, ResponseCode.SUCCESS, false, 'Temporary Password is sented.');
 		} else {
 			throw new InternalServerErrorException(`Password Reset Failed. Internal Server error. Plz contact server admin`);
 		}
-	}
-
-	async getAllUsers(): Promise<ResponseDto> {
-		const userList: User[] = await this.userRepository.selectAllUsers();
-		return new ResponseDto(constant.HttpStatus.OK, undefined, { list: userList });
 	}
 }

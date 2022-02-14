b906b2a0ab911d912828cf8fa0b74c57e1f9ec72
JangLuna <75708535+JangLuna@users.noreply.github.com>
2022-02-14 16:39:09 +0900

    feature: Profile controller 추가, Global pipes 추가 (#36)

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ResponseDto } from 'src/dto/response.dto';
 import { ProfileDto } from './dto/profile.dto';
 import { ProfileService } from './profile.service';
 import Constant from 'src/response.constant';
 import { ResponseCode } from 'src/response.code.enum';
+import GlobalPipes from '../pipes/global-pipes.pipe';
 
 @Controller('profile')
 @UseGuards(AuthGuard())
@@ -17,6 +18,7 @@ export class ProfileController {
 	}
 
 	@Post('/create_profile')
+	@UsePipes(ValidationPipe)
 	createProfile(@Req() req, @Body() profileDto: ProfileDto): Promise<ResponseDto> {
 		// 토큰으로 부터 받은 유저 idx
 		const requsetUserIdx = req.user.idx;
@@ -24,6 +26,7 @@ export class ProfileController {
 	}
 
 	@Post('/update_profile')
+	@UsePipes(ValidationPipe)
 	updateProfile(@Req() req, @Body() profileDto: ProfileDto): Promise<ResponseDto> {
 		// 토큰으로 부터 받은 유저 idx
 		const requsetUserIdx = req.user.idx;

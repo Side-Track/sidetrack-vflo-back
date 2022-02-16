17444d4452ec99d7a00e851a86dd3d62a22d34a9
JangLuna <75708535+JangLuna@users.noreply.github.com>
2022-02-16 14:19:02 +0900

    fix: exception errors (#48)

--- src/response.message.enum.ts
+++ src/response.message.enum.ts
@@ -6,10 +6,12 @@ export enum ResponseMessage {
 
 	ALREADY_REGISTERED_USER = '이미 가입된 유저입니다.',
 	NOT_REGISTERED_USER = '존재하지 않는 계정입니다.',
+	UNAUTHORIZED_USER = '권한이 없습니다.',
 
 	ALREADY_VERIFIED_USER = '이미 인증된 계정입니다.',
 	WRONG_EMAIL_OR_PASSWORD = '잘못된 이메일 또는 비밀번호 입니다.',
 
 	NOT_VERIFIED_EMAIL = '인증되지 않은 이메일입니다.',
 	MAILER_ERROR = '메일 발송에 실패했습니다.',
+	ALREADY_EXIST_GENRE = '이미 존재하는 장르입니다.',
 }
